206284e4002e230d32b2423d4a0390c67af8f8b1
ANDRES TALERO <andres_alvarado@cun.edu.co>
2025-07-12 18:42:17 -0500

    fix algunas rutas en securityconfig y ajuste cors y  en usurio

---
+++ src/main/java/com/anfealta/ecommerce/ecomerce_backend/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.anfealta.ecommerce.ecomerce_backend.config; // Asegúrate de que el paquete sea correcto
+
+/*
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Aplica CORS a todas las rutas bajo /api
+                .allowedOrigins("http://localhost:4200") // ¡Permite solicitudes desde tu frontend de Angular!
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
+                .allowedHeaders("*") // Permite todos los headers en la solicitud
+                .allowCredentials(true) // Importante si usas cookies o sesiones
+                .maxAge(3600); // Duración en segundos que la respuesta de pre-flight puede ser cacheada
+    }
+}
+    */
